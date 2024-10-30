c5feb0e472c74f2e7139417990dd6d38ccf78833
caiomvital <caiomvital@gmail.com>
2024-10-30 16:03:56 -0300

    commit do projeto do dia 30/10/24

---
+++ 24-10-30/src/listas/ArrayLists.java
@@ -0,0 +1,12 @@
+package listas;
+
+import java.util.Random;
+
+public class ArrayLists {
+public static void main(String[] args) {
+	Random r = new Random();
+
+	System.out.print(r.nextInt(10));
+	
+}
+}
