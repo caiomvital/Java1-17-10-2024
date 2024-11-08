0fa57f7f273b84ebcfe162e90511eb07e2cd55f6
caiomvital <caiomvital@gmail.com>
2024-11-08 10:01:27 -0300

    projeto do dia 07/11/24 enviado

---
+++ 24-11-07/src/encapsulamento/Pessoa.java
@@ -0,0 +1,41 @@
+package encapsulamento;
+
+public class Pessoa {
+
+	private String nome;
+	private String telefone;
+	private String cpf;
+	private double altura;
+
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+	
+}
