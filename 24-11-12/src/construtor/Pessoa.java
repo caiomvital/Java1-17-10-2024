d1f07b3992c9f73f8812119d10240a366cf61dc9
caiomvital <caiomvital@gmail.com>
2024-11-13 14:18:53 -0300

    projeto 24-11-12 adicionado

---
+++ 24-11-12/src/construtor/Pessoa.java
@@ -0,0 +1,21 @@
+package construtor;
+
+public class Pessoa {
+
+	String nome;
+	String cpf;
+	
+	public Pessoa() {}
+	
+	public Pessoa(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	
+	}
+	
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
