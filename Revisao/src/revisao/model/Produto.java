e7c2b71dc39d1f30d70071fa417c0920f639dbe1
caiomvital <caiomvital@gmail.com>
2024-11-21 13:57:01 -0300

    projeto da revisao do dia 20/11/24

---
+++ Revisao/src/revisao/model/Produto.java
@@ -0,0 +1,16 @@
+package revisao.model;
+
+public class Produto {
+
+	public String nome;
+	public double preco;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
